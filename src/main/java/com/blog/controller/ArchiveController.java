50c8a2587a95b9e0468d202622641584de9c7df1
xi_nian <1617171230@qq.com>
2020-11-23 19:02:15 +0800

    归档-0.21

--- src/main/java/com/blog/controller/ArchiveController.java
+++ src/main/java/com/blog/controller/ArchiveController.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @Description: 归档信息统计
@@ -23,10 +27,42 @@ public class ArchiveController {
     private ArchiveService archiveService;
 
     @GetMapping("/archive")
-    public String toArchive(Model model){
+    @ResponseBody
+    public void toArchive(Model model){
         List<Archive> allArchive = archiveService.findAllArchive();
+        Map<String,List<Archive>> map = new HashMap<>();
+        String archiveTime = null;
+        //key存储的是年月份，如果重复就让该key的value中再存储一条archive
+        for (Archive archive : allArchive) {
+            archiveTime = archive.getYear()+"年"+archive.getMonth()+"月";
+            map.put(archiveTime, (List<Archive>) archive);
+        }
+
+
+        List<String> timeList = new ArrayList<>();
         model.addAttribute("allArchive",allArchive);
-        return "archive";
+        String yearMonth = null;
+        for (Archive archive : allArchive) {
+            //year=2019, month=10, day=03, articleTitle=Java基础总结, articleId=1002
+            /**
+             * 2019年 12月 3 篇
+             *      12-05栈和队列
+             *      12-15图解数据结构
+             *      12-28算法入门
+             */
+            yearMonth = archive.getYear() +"年"+ archive.getMonth()+"月";
+            for (String time : timeList) {
+                if(yearMonth.equals(time)){
+
+                }
+            }
+            timeList.add(yearMonth);
+
+            System.out.println(archive);
+        }
+        for (String s : timeList) {
+            System.out.println(s);
+        }
     }
 
 }
--- src/main/java/com/blog/entity/Archive.java
+++ src/main/java/com/blog/entity/Archive.java
@@ -1,12 +1,21 @@
 package com.blog.entity;
 
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * @Description: 归档实体
  * @Author : 王俊
  * @date :  2020/11/20
  */
+@Data
+@ToString
 public class Archive {
 
-
+    private String year;    //年份
+    private String month;   //月份
+    private String day;     //几号
+    private String articleTitle;    //文章名称
+    private Integer articleId;      //文章id
 
 }
